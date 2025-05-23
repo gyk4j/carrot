eaf70041b0cc1f57da79946ae3516a7b5b20f68f
gyk <147011991+gyk4j@users.noreply.github.com>
2025-05-23 14:11:28 +0800

    Add pyvips test (#5)

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import tkinter as tk
+import tkinter.filedialog as fd
+import pyvips
 
+def thumbnail():
+  input = fd.askopenfilename(title="Select Picture", filetypes=[("JPEG images", ('*.jpg')), ("All files", "*.*")]) 
+  thumb = pyvips.Image.thumbnail(input, 128, height=128)
+  thumb.write_to_file('tn-test.jpg')
+  
 def close_window(): 
   root.destroy()
 
@@ -10,8 +17,11 @@ frame = tk.Frame(root)
 frame.pack()
 
 # place a label on the root window
-message = tk.Label(frame, text="Hello, World!")
+message = tk.Label(frame, text=pyvips.API_mode)
 message.pack()
+
+btn_thumbnail = tk.Button (frame, text = "Thumbnail", command = thumbnail)
+btn_thumbnail.pack()
 
 button = tk.Button (frame, text = "Good-bye.", command = close_window)
 button.pack()
