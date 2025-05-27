430d423ae94477bcf4c7fd71966f8279d9e397f1
gyk4j <147011991+gyk4j@users.noreply.github.com>
2025-05-27 10:17:39 +0800

    Add setup.py for setuptools

---
+++ src/car/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='car',
+    version='0.0.1',
+    description='Crop and Resize package',
+    author='gyk4j',
+    author_email='147011991+gyk4j@users.noreply.github.com',
+    packages=['car'],
+    install_requires=[
+    ],
+)
