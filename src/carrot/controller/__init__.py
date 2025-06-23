9217495f0890484d8167159e818bf5565933bcee
gyk4j <147011991+gyk4j@users.noreply.github.com>
2025-06-23 12:08:00 +0800

    Add MVC scaffolding placeholders

---
+++ src/carrot/controller/__init__.py
@@ -0,0 +1,5 @@
+# Define the __all__ variable
+__all__ = ["controller"]
+
+# Import the submodules
+from . import controller
---
+++ src/carrot/model/__init__.py
@@ -0,0 +1,5 @@
+# Define the __all__ variable
+__all__ = ["model"]
+
+# Import the submodules
+from . import model
