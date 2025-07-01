bdb1a41af3ad5b05b2071582fa7c1457f8d0f760
gyk <147011991+gyk4j@users.noreply.github.com>
2025-07-01 10:35:32 +0800

    Add image fitting to window resize event (#39)

--- src/carrot/view/imageview.py
+++ src/carrot/view/imageview.py
@@ -18,6 +18,8 @@ class ImageView:
         self.load_image()
         print("%d x %d" % (self.imageview.winfo_width(), self.imageview.winfo_height()))
 
+        self.imageview.bind("<Configure>", self.on_configure)
+
     def load_image(self):
         file = "%d.jpg" % (self.current_img)
         #print(file)
@@ -26,16 +28,16 @@ class ImageView:
 
     def fit_image(self):
         i_width, i_height = self.img_original.size
-        c_width = self.imageview.winfo_width()
-        c_height = self.imageview.winfo_height()
+        self.c_width = self.imageview.winfo_width()
+        self.c_height = self.imageview.winfo_height()
 
         print("Image Width     : %d pixels" % (i_width))
         print("Image Height    : %d pixels" % (i_height))
-        print("Canvas Width    : %d pixels" % (c_width))
-        print("Canvas Height   : %d pixels" % (c_height))
+        print("Canvas Width    : %d pixels" % (self.c_width))
+        print("Canvas Height   : %d pixels" % (self.c_height))
 
-        aspect_ratio_h = c_width / i_width
-        aspect_ratio_v = c_height / i_height
+        aspect_ratio_h = self.c_width / i_width
+        aspect_ratio_v = self.c_height / i_height
         print("Aspect ratio (w): %f" % (aspect_ratio_h))
         print("Aspect ratio (h): %f" % (aspect_ratio_v))
         aspect_ratio = min( aspect_ratio_h, aspect_ratio_v )
@@ -49,12 +51,34 @@ class ImageView:
         self.img = ImageTk.PhotoImage(self.img_resized)
         
         if self.img_id != -1:
-            self.imageview.delete( id = self.img_id )
+            self.imageview.delete( self.img_id )
 
-        x = (c_width - resized_width) // 2
-        y = (c_height - resized_height) // 2
+        x = (self.c_width - resized_width) // 2
+        y = (self.c_height - resized_height) // 2
         self.img_id = self.imageview.create_image(x, y, anchor=tk.NW, image = self.img)
             
+    def on_configure(self, event):
+        print(event)
+        width_latest = event.width
+        height_latest = event.height
+
+        # Assume no change, just repeated duplicate event.
+        dirty: bool = False
+
+        # Update only if there is any change.
+        if width_latest != self.c_width:
+            self.c_width = width_latest
+            dirty = True
+
+        # Update only if there is any change.
+        if height_latest != self.c_height:
+            self.c_height = height_latest
+            dirty = True
+
+        # Only print if size is changed.
+        if dirty:
+            print("%d x %d" % (self.c_width, self.c_height))
+            self.fit_image()
 
     def next(self):
         if self.current_img < ImageView.MAX:
