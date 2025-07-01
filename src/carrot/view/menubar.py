27475de6fe0aa53b8ef6b36274be9a40989f462f
gyk <147011991+gyk4j@users.noreply.github.com>
2025-07-01 09:59:48 +0800

    Add Go menu and File separator (#38)

--- src/carrot/view/menubar.py
+++ src/carrot/view/menubar.py
@@ -10,7 +10,16 @@ class MenuBar:
         window.config(menu=self.menubar)
         
         self.menu_file = Menu(self.menubar, "File")
+        self.menu_file_new = MenuItem(self.menu_file.menu, "New")
+        self.menu_file_open = MenuItem(self.menu_file.menu, "Open")
+        self.menu_file_save = MenuItem(self.menu_file.menu, "Save")
+        self.menu_file_saveas = MenuItem(self.menu_file.menu, "Save as")
+        self.menu_file_separator = MenuItem(self.menu_file.menu, "-")
         self.menu_file_exit = MenuItem(self.menu_file.menu, "Exit")
+
+        self.menu_go = Menu(self.menubar, "Go")
+        self.menu_go_back = MenuItem(self.menu_go.menu, "Back")
+        self.menu_go_next = MenuItem(self.menu_go.menu, "Next")
         
         self.menu_help = Menu(self.menubar, "Help")
         self.menu_help_about = MenuItem(self.menu_help.menu, "About")
