b85b05fef089778e1c2c131fc986c9e7920a6bf3
gyk4j <147011991+gyk4j@users.noreply.github.com>
2025-07-10 08:43:31 +0800

    Add menu item icons

--- src/carrot/view/menubar.py
+++ src/carrot/view/menubar.py
@@ -11,19 +11,19 @@ class MenuBar:
         self._window.config(menu=self._menubar)
         
         self._menu_file = Menu(self._menubar, "File")
-        self._menu_file_new = MenuItem(self._menu_file.menu, "New")
-        self._menu_file_open = MenuItem(self._menu_file.menu, "Open")
-        self._menu_file_save = MenuItem(self._menu_file.menu, "Save")
-        self._menu_file_saveas = MenuItem(self.menu_file.menu, "Save as")
+        self._menu_file_new = MenuItem(self._menu_file.menu, "New", "res/ico/file_new_24.png")
+        self._menu_file_open = MenuItem(self._menu_file.menu, "Open", "res/ico/file_open_24.png")
+        self._menu_file_save = MenuItem(self._menu_file.menu, "Save", "res/ico/file_save_24.png")
+        self._menu_file_saveas = MenuItem(self.menu_file.menu, "Save as", "res/ico/file_saveas_24.png")
         self._menu_file_separator = MenuItem(self._menu_file.menu, "-")
-        self._menu_file_exit = MenuItem(self._menu_file.menu, "Exit")
+        self._menu_file_exit = MenuItem(self._menu_file.menu, "Exit", "res/ico/file_exit_24.png")
 
         self._menu_go = Menu(self._menubar, "Go")
-        self._menu_go_back = MenuItem(self._menu_go.menu, "Back")
-        self._menu_go_next = MenuItem(self._menu_go.menu, "Next")
+        self._menu_go_back = MenuItem(self._menu_go.menu, "Back", "res/ico/go_back_24.png")
+        self._menu_go_next = MenuItem(self._menu_go.menu, "Next", "res/ico/go_forward_24.png")
         
         self._menu_help = Menu(self._menubar, "Help")
-        self._menu_help_about = MenuItem(self._menu_help.menu, "About")
+        self._menu_help_about = MenuItem(self._menu_help.menu, "About", "res/ico/help_about_24.png")
 
     @property
     def window(self) -> tk.Toplevel:
